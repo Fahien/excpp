0a05c266ca0926c4f41dc690ec4ed9f039254631
Antonio Caggiano <info@antoniocaggiano.eu>
2020-01-30 19:55:02 +0100

    gfx: ImageView

--- include/graphics/image.h
+++ include/graphics/image.h
@@ -18,8 +18,27 @@ class Image
 	Image& operator=( Image&& o );
 
 	Device& device;
+	VkFormat format = VK_FORMAT_UNDEFINED;
+
 	VkImage handle = VK_NULL_HANDLE;
 	VkDeviceMemory memory = VK_NULL_HANDLE;
 };
+
+
+class ImageView
+{
+  public:
+	ImageView( Device& d, Image& i );
+	~ImageView();
+
+	ImageView( ImageView&& o );
+	ImageView& operator=( ImageView&& o );
+
+	Device& device;
+	Image& image;
+
+	VkImageView handle = VK_NULL_HANDLE;
+};
+
 
 } // namespace graphics
--- src/image.cpp
+++ src/image.cpp
@@ -25,6 +25,7 @@ VkFormat get_format( Png& png )
 
 Image::Image( Device& d, Png& png )
 : device { d }
+, format { get_format( png ) }
 {
 	// Image
 	VkImageCreateInfo info = {};
@@ -36,7 +37,7 @@ Image::Image( Device& d, Png& png )
 	info.mipLevels = 1;
 	info.arrayLayers = 1;
 	info.tiling = VK_IMAGE_TILING_OPTIMAL;
-	info.format = get_format( png );
+	info.format = format;
 	info.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
 	info.usage = VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT;
 	info.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
@@ -78,6 +79,7 @@ Image::~Image()
 
 Image::Image( Image&& other )
 : device { other.device }
+, format { other.format }
 , handle { other.handle }
 , memory { other.memory }
 {
@@ -89,8 +91,57 @@ Image::Image( Image&& other )
 Image& Image::operator=( Image&& other )
 {
 	assert( device.handle == other.device.handle && "Cannot move images from different device" );
+	std::swap( format, other.format );
 	std::swap( handle, other.handle );
 	std::swap( memory, other.memory );
+
+	return *this;
+}
+
+
+ImageView::ImageView( Device& d , Image& i )
+: device { d }
+, image { i }
+{
+	VkImageViewCreateInfo info = {};
+	info.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+	info.image = image.handle;
+	info.viewType = VK_IMAGE_VIEW_TYPE_2D;
+	info.format = image.format;
+	info.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+	info.subresourceRange.baseMipLevel = 0;
+	info.subresourceRange.levelCount = 1;
+	info.subresourceRange.baseArrayLayer = 0;
+	info.subresourceRange.layerCount = 1;
+
+	auto res = vkCreateImageView( device.handle, &info, nullptr, &handle );
+	assert( res == VK_SUCCESS && "Cannot create image view" );
+}
+
+
+ImageView::~ImageView()
+{
+	if ( handle != VK_NULL_HANDLE )
+	{
+		vkDestroyImageView( device.handle, handle, nullptr );
+	}
+}
+
+
+ImageView::ImageView( ImageView&& other )
+: device { other.device }
+, image { other.image }
+, handle { other.handle }
+{
+	other.handle = VK_NULL_HANDLE;
+}
+
+
+ImageView& ImageView::operator=( ImageView&& other )
+{
+	assert( device.handle == other.device.handle && "Cannot move images from different device" );
+	assert( image.handle == other.image.handle && "Cannot move image views from different images" );
+	std::swap( handle, other.handle );
 
 	return *this;
 }
