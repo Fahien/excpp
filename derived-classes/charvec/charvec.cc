e20fb043d4cbb392b276abf2f7b8d378dbd8301f
Antonio Caggiano <info@antoniocaggiano.eu>
2021-01-12 00:59:33 +0100

    Derived classes
    Charvec and Task

---
+++ derived-classes/charvec/charvec.cc
@@ -0,0 +1,39 @@
+#include <cstdio>
+
+/// Consider this class, define new_charvec to allocate contiguous memory for a CharVec object so
+/// that the elements can be indexed through element as shown. Under what circumstances does this
+/// trick cause serious problems?
+/// Considering this exercise is in the derived classes section, the answer would be probably
+/// related to implementing a CharVec subclass, which by storing its own attributes in the area
+/// of memory next to element, would overlap the same area of memory dedicated to elements.
+class CharVec {
+  public:
+    static CharVec* new_charvec(int s);
+
+    char& operator[](int i) { return element[i]; }
+
+    private:
+    int size;
+    char element[1];
+};
+
+CharVec* CharVec::new_charvec(int s) {
+    void* mem = ::operator new(sizeof(CharVec) + (s - 1) * sizeof(element[0]));
+
+    auto ret = reinterpret_cast<CharVec*>(mem);
+    ret->size = s;
+
+    return ret;
+}
+
+int main()
+{
+    auto charvec = CharVec::new_charvec(10);
+
+    for(size_t i = 0; i < 10; ++i) {
+        (*charvec)[i] = 'a' + i;
+        std::printf("%c ", (*charvec)[i]);
+    }
+
+    return 0;
+}
---
+++ derived-classes/task/task.cc
@@ -0,0 +1,41 @@
+#include "task.h"
+
+namespace excpp {
+
+void print_slowly(const std::string& message)
+{
+    int64_t time = 0;
+    int64_t delay_between_characters = 100000;
+
+    auto tasks = std::vector<std::unique_ptr<Task>>();
+
+    for(auto c : message) {
+        std::unique_ptr<Task> task = std::make_unique<Print>(c);
+        task->delay(time);
+        tasks.push_back(std::move(task));
+        // More delay if it is a comma
+        if (c == ',') {
+            time += 16 * delay_between_characters;
+        } else {
+            time += delay_between_characters;
+        }
+    }
+
+    auto scheduler = Scheduler();
+
+    for(auto& task : tasks) {
+        scheduler.add(*task);
+    }
+
+    for (size_t t = 0; t < time; ++t) {
+        scheduler.run(1);
+    }
+
+}
+
+} // namespace excpp
+
+int main() {
+    excpp::print_slowly("Hello, this is computer!\n");
+    return 0;
+}
